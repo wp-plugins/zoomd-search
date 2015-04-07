0a463bc9ae1e4df4d23291c61a482ce75d9acac1
Zoomd <Zoomd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 14:53:31 +0000

    uploading version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/zoomd-search/trunk@1129645 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,165 @@
+<?PHP
+/*
+Plugin name: Zoomd Search
+Plugin URI: http://zoomd.com/
+Description: A beautiful search experience, that engages your visitors, improves conversion and adds monetization to your site
+Author: Zoomd
+Author URI: http://zoomd.com/
+License: GPLv2 or later
+Version: 1.1
+*/
+
+session_start();
+
+add_action('wp_footer', 'add_spherupPlugin');
+function add_spherupPlugin() {
+    $clientId  = get_option('zoomd_clientId');
+    if( $clientId ) {
+        wp_register_script( 'zoomd_search', plugins_url( 'js/sphereup.widget.min.js', __FILE__ ), array('jquery'), 1, true);
+        wp_enqueue_script('zoomd_search');
+        ?>
+        <!-- START SPHEREUP -->
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                if(typeof SphereUp != 'undefined' && typeof SphereUp.SphereUpWidget != 'undefined') {
+                    SphereUp.SphereUpWidget.loadWidget({clientId: '<?php echo $clientId ?>'});
+                }
+            });
+        </script>
+        <!-- END SPHEREUP -->
+    <?php
+    }
+ }
+
+function zoomd_plugin_deactivation() {
+    delete_option('zoomd_siteurl');
+    delete_option('zoomd_clientId');
+    delete_option('zoomd_email');
+    delete_option('zoomd_password');
+    delete_option('zoomd_sphereup_admin_url');
+    delete_option('zoomd_deferred_admin_notices');
+}
+
+register_deactivation_hook(__FILE__, 'zoomd_plugin_deactivation');
+
+function zoomd_plugin_settings() {
+
+    add_options_page( __( "ZoomD Plugin settings", BSEARCH_LOCAL_NAME ), __( "Zoomd Search", BSEARCH_LOCAL_NAME ), 'manage_options', 'zoomd_settings', 'zoomd_display_settings');
+}
+add_action( 'admin_menu', 'zoomd_plugin_settings' );
+
+function zoomd_action_links( $links ) {
+    $newField = array();
+    $newField[] = '<a href="'. get_admin_url(null, 'options-general.php?page=zoomd_settings') .'">Settings</a>';
+    $newField = array_merge($newField, $links);
+    return $newField;
+}
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'zoomd_action_links' );
+
+
+add_action('admin_notices', 'zoomd_admin_notices');
+function zoomd_admin_notices() {
+    if ($notices= get_option('zoomd_deferred_admin_notices')) {
+        foreach ($notices as $notice) {
+            echo "<div class='error'><p>$notice</p></div>";
+        }
+        delete_option('zoomd_deferred_admin_notices');
+    }
+}
+
+function zoomd_display_settings() {
+    $siteurl = (get_option('zoomd_siteurl') != '') ? get_option('zoomd_siteurl') :  get_option('siteurl');
+    $clientId  = get_option('zoomd_clientId');
+    $email  = (filter_var(get_option('zoomd_email'), FILTER_VALIDATE_EMAIL)) ? get_option('zoomd_email') : get_option('admin_email');
+    $password  = (get_option('zoomd_password') != '') ? get_option('zoomd_password') : '';
+    $first_name  = (get_option('blogname') != '') ? get_option('blogname') : 'ZoomDPlugin';
+    $second_name  = (get_option('siteurl') != '') ? get_option('siteurl') : 'ZoomDPlugin';
+    $adminUrl  = (get_option('zoomd_sphereup_admin_url') != '') ? get_option('zoomd_sphereup_admin_url') : false;
+
+    $updateAdminLink = false;
+
+    if ( get_option('zoomd_clientId_email') != $email || !$clientId || get_option('zoomd_clientId_siteUrl') != $siteurl ) {
+        $updateAdminLink = true;
+    }
+
+    if($updateAdminLink && $password) {
+        $jsonurl = "https://suadmin-ss.sphereup.com/SelfService/Register?firstName=" . urlencode($first_name) . "&lastName=" . urlencode($second_name) . "&email=" . urlencode($email) . "&password=" . urlencode($password) . "&url=" . urlencode($siteurl);
+
+        $json = file_get_contents($jsonurl);
+        $result = json_decode($json);
+
+        if(isset($result->error)) {
+
+            update_option('zoomd_deferred_admin_notices', array("Error retturned from the service: ". $result->error) );
+        }
+
+        if(isset($result->clientId)) {
+            $clientId = $result->clientId;
+            update_option('zoomd_clientId', $result->clientId);
+            update_option('zoomd_clientId_email', $email);
+            update_option('zoomd_sphereup_admin_url', 'http://www.zoomd.com/#login');
+            $adminUrl = 'http://www.zoomd.com/#login';
+        }
+    }
+
+    $additionalData = 'style="width: 250px;"';
+    if($adminUrl) {
+        $additionalData .= ' disabled="disabled"';
+    }
+
+    $html = '<div class="wrap">
+
+            <form method="post" name="options" action="options.php">
+
+            <h2>Zoomd Account</h2>' . wp_nonce_field('update-options') . '
+            <table width="100%" cellpadding="10" class="form-table">';
+
+    if(!$clientId || !$password) {
+        $html .= '<tr>
+                    <td align="left" scope="row" style="color: red;" colspan="2">
+                        Your plugin is currently not set up.<br/><br/>Please enter the details below and click Save. Don\'t forget to choose a password too!
+                    </td>
+                </tr>';
+    }
+
+    $html .= '<tr>
+                    <td align="left" style="max-width: 200px;" width="200">
+                        <label>Your Website URL: </label>
+                    </td>
+                    <td align="left">
+                        <input type="text" name="zoomd_siteurl" value="' . $siteurl . '" '.$additionalData.' required/>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left">
+                        <label>Your Email Address: </label>
+                    </td>
+                    <td align="left" scope="row">
+                        <input type="email" name="zoomd_email" value="' . $email . '" '.$additionalData.' required/>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left">
+                        <label>Create a Password: </label>
+                    </td>
+                    <td align="left" scope="row">
+                        <input type="password" name="zoomd_password" value="' . $password . '" '.$additionalData.' required title="3 characters minimum" pattern=".{3,}"/>
+                    </td>
+                </tr>';
+
+    if( $adminUrl ) {
+        $html .= '<tr><td align="left" scope="row" colspan="2">Thanks you for creating a Zoomd Search account!<br/>Visit the Admin page <a href="'.$adminUrl.'" target="_blank">HERE</a></td></tr>';
+    }
+    $html .= '</table>';
+
+    if ( !$adminUrl ) {
+        $html .= '<p class="submit">
+                <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="page_options" value="zoomd_siteurl,zoomd_email,zoomd_password" />
+                <input type="submit" name="Submit" value="Save" />
+            </p>';
+
+    }
+    $html .= '</form></div>';
+    echo $html;
+}
---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Zoomd Search ===
+Contributors: Zoomd
+Tags: search, better search, visual search, smart search, monetization, actionable search, engaging, mini site, zoomd, monetize site search 
+Requires at least: 4.1.0
+Tested up to: 4.1.1
+Stable tag: 4.1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Zoomd Search is a monetizable site search – intuitive, beautiful and visually rich
+
+== Description ==
+Zoomd Search gives your visitors a beautiful search experience, to help them find what they are looking for, easily and efficiently. It engages your visitors, improves conversion and adds monetization to your site. 
+With a look that stands out, your site search will no longer be ignored, and you’ll be able to offer your visitors a great exploration experience, by presenting them with rich search results that highlight your content. Search results will appear in an efficient and attractive format, presenting your pages’ content in a newly created space, where you will also be able to show ads, promoted content or use one of our monetization options, if you choose to do so. Zoomd also upgrades your search with smart search functionalities, such as search-as-you-type, spell checker and more.
+We are hard at work to make our search experience even more customizable and to offer you more monetization options that will best match your site’s spirit and your visitors.
+*Hey, it’s your content – we just make it more visible & accessible to your visitors*
+
+= Key Features =
+* **Monetizable site search**: Offer your visitors non-intrusive, contextual ads inside the search results.
+* **Visual search results**: Get rich and engaging search results that include images, links, videos and text.
+* **Smart search**: Get advanced search capabilities that work in any language: Search as you type, auto-correct, auto-suggest.
+* **Ongoing site updates**: Website syncs for maximum accuracy of search results.
+* **Engaging Exploration**: Get search results that create a mini-site experience, with action driven content and a fresh set of monetization options.
+* **Actionable search**: Let your visitors call & send email directly from search results.
+* **Search reports**: Powerful, easy-to-use management dashboard offering detailed reports and analytics, usage trends, and queries analysis.
+
+== Installation ==
+1. **Install**: In your WordPress admin panel, go to Plugins > New Plugin, search for Zoomd Search and click "Install now”. Alternatively, download the plugin and upload the contents of ZoomdSearch to your plugins directory, which usually is /wp-content/plugins/.
+2. **Activate**: Activate the Zoomd Search plugin through the 'Plugins' section in the WordPress admin.
+3. **Settings**: Click on Settings (in Zoomd Search, under the 'Plugins' section) and create a Zoomd account. Enter your *Website URL*, your *Email Address* and a *Password*, and click Save.
+4. **Configure**: Follow the link that appears in the Zoomd Account page to customize your Zoomd Search and to view usage reports.
+
+== Frequently Asked Questions ==
+= Can I get Zoomd Search on non WordPress websites? =
+Yes, you can. The installation is fast and simple, and you can even see a preview in just a few seconds. [Click here](http://www.zoomd.com/register.html) to learn more.
+= How can I see the Search Reports? =
+Go to [Zoomd.com](http://zoomd.com), log into the administration dashboard (Login button on the top right), and open the Reports tab.
+= Can I influence the sorting of the search results? =
+Zoomd Search automatically rates the results based on relevancy, date, context, etc. – this is our secret sauce.
+= How can I customize the look & feel of my search? =
+Go to [Zoomd.com](http://zoomd.com), log into the administration dashboard (Login button on the top right), and open the Design tab
+
+== Screenshots ==
+1. Let your visitors find what they need
+2. Integrate CALL and EMAIL functionality
+3. A variety of beautiful designs
+
+== Changelog ==
+= 1.1 =
+* Bug fixes
+* Adding customization and reports view in Zoomd admin
+
+= 1.0 =
+* First release
